a25e4d279da8d744885e6212ec53acd99f60f43a
nastr00t <elvis.acosta@gmail.com>
2024-07-29 07:03:21 -0500

    final

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import Navbar from './Navbar'
+import NProgress from "nprogress"; // Importa NProgress para la barra de progreso
+import nProgress from "nprogress"; // Importa nProgress, parece un error de duplicación
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-const Layout = ({ children }) => {
+
+const Layout = ({ children,footer=true }) => {
+  
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRoutechange = (url) =>
+    {
+      NProgress.start();
+     };
+  });
+  
   return (
     <>
       <Navbar />
-
       {children}
     </>
   )
 }
-
-export default Layout
+Layout.PropTypes =
+{
+  children: PropTypes.node,
+  footer:PropTypes.bool
+};
+export default Layout;
